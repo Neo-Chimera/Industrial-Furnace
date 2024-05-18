aceaa83687b43b774a93fe60a52fc1dca3ad34d6
B00TT3R <79879915+B00TT3R@users.noreply.github.com>
2024-05-18 17:04:55 -0400

    add multitasking

--- startup.lua
+++ startup.lua
@@ -1,14 +1,11 @@
 os.loadAPI("feedFurnace.lua")
 os.loadAPI("moveToChest.lua")
 os.loadAPI("addToFurnaces.lua")
-local feedFurnacesLoop = feedFurnace.feedFurnacesLoop
+local feedAllFurnaces = feedFurnace.feedAllFurnaces
 local moveToChest = moveToChest.moveToChestLoop
 local addToAllFurnaces = addToFurnaces.addToAllFurnaces
 
 print("Bot 64")
 print("Computador central da fornalha automática")
-parallel.waitForAll(addToAllFurnaces, feedFurnacesLoop, moveToChest)
---[[ while true do
-    sleep(0.05)
-     
-end ]]
+parallel.waitForAll(addToAllFurnaces, feedAllFurnaces, moveToChest)
+
