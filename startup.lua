8a836e9ed55ee6ada54542c0cb7b423023e32f41
B00TT3R <79879915+B00TT3R@users.noreply.github.com>
2024-05-20 20:42:10 -0400

    multitasking

--- startup.lua
+++ startup.lua
@@ -1,11 +1,24 @@
 os.loadAPI("feedFurnace.lua")
 os.loadAPI("moveToChest.lua")
 os.loadAPI("addToFurnaces.lua")
-local feedAllFurnaces = feedFurnace.feedAllFurnaces
-local moveToChestAll = moveToChest.moveToChestAll
-local addToAllFurnaces = addToFurnaces.addToAllFurnaces
+local allGenericFurnaces = getFurnaces.getGenericFurnaces()
+
+local addToSelf = addToFurnaces.addToSelf
+local moveToChestSingle = moveToChest.moveToChestSingle
+local feedSingleFurnace = feedFurnace.feedSingleFurnace
 
 print("Bot 64")
 print("Computador central da fornalha automática")
-parallel.waitForAll(addToAllFurnaces, feedAllFurnaces, moveToChestAll)
 
+
+local tasks = {}
+for _, furnace in pairs(allGenericFurnaces) do
+    table.insert(tasks, function() 
+        parallel.waitForAll(
+            function() addToSelf(furnace) end,
+            function() moveToChestSingle(furnace) end,
+            function() feedSingleFurnace(furnace) end
+        )
+    end)
+end
+parallel.waitForAll(table.unpack(tasks))
