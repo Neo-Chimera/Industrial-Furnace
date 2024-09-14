e551bb4b98b54ac23adabf475e23853dc77e76d4
B00TT3R <79879915+B00TT3R@users.noreply.github.com>
2024-09-14 15:02:35 -0400

    Create invProvider.lua

---
+++ invProvider.lua
@@ -0,0 +1,4 @@
+fuelChest = peripheral.wrap("sophisticatedstorage:barrel_10")
+materialChest = peripheral.wrap("sophisticatedstorage:barrel_9")
+resultChest = peripheral.wrap("sophisticatedstorage:barrel_8")
+bucketStorage = peripheral.wrap("sophisticatedstorage:barrel_8")
